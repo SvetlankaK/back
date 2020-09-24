480947f1a14db7f27815967c44c80b40ffa6b4c0
Alex <aleks.kvetko@gmail.com>
2020-09-24 14:44:01 +0300

    безумно можно быть пееервым

--- src/main/java/com/svetakvetko/config/SpringFoxConfig.java
+++ src/main/java/com/svetakvetko/config/SpringFoxConfig.java
@@ -3,16 +3,30 @@ package com.svetakvetko.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+      Parameter headers = new ParameterBuilder()
+          .name("Authorization") // name of the header
+          .modelRef(new ModelRef("string")) // data-type of the header
+          .required(false) // required/optional
+          .parameterType("header") // for query-param, this value can be 'query'
+          .description("Auth Token")
+          .build();
+      return new Docket(DocumentationType.SWAGGER_2)
+                .forCodeGeneration(true)
+                .globalOperationParameters(Collections.singletonList(headers))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
