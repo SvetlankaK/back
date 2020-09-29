77d43fffe2f96f80305859740a3ff8ffb28ab628
SvetlankaK <svetlanaka17@hmail.com>
2020-09-29 16:51:09 +0300

    здоровья погибшим

--- src/main/java/com/svetakvetko/domain/User.java
+++ src/main/java/com/svetakvetko/domain/User.java
@@ -59,21 +59,28 @@ public class User implements UserDetails {
         return userLogin;
     }
 
+
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+
+    @JsonIgnore
     @Override
     public boolean isEnabled() {
         return true;
